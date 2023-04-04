66e5de302131e2e6ff2815b6eb5d613ce928887a
Joao Pedro Goulart <joaopedromgoulart@gmail.com>
2023-04-04 00:09:41 -0300

    Add source environment

---
+++ tests/sources/test_environment.py
@@ -0,0 +1,21 @@
+import mock
+import pytest
+import os
+from config_joker.sources.environment import EnvironmentSource
+
+
+class TestEnvironmentSource:
+    @mock.patch.dict(os.environ, {"rigth_key": "value"})
+    @pytest.mark.parametrize(
+        'key, expected_value, exists',
+        [
+            ('rigth_key', 'value', True),
+            ('wrong_key', None, False)
+        ]
+    )
+    def test_existing_key(self, key, expected_value, exists):
+        environment_source = EnvironmentSource()
+        source_response = environment_source.get_value(key)
+        assert source_response.value == expected_value
+        assert source_response.exists == exists
+    
