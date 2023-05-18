98a7e4561a525ac3156963d78c13b19ca5e870a9
Joao Pedro Goulart <joaopedromgoulart@gmail.com>
2023-05-17 22:30:48 -0300

    refactor(sources.source-sources.yamlfile): Base class to sources thet cam be transformed to dicts

--- config_joker/sources/yamlfile.py
+++ config_joker/sources/yamlfile.py
@@ -1,27 +1,12 @@
 from yaml import safe_load
 
-from config_joker.sources.source import Source, SourceResponse
-from config_joker.utils.parser import dict_extractor
+from config_joker.sources.source import SourceAsDict
 
 
-class YamlFileSource(Source):
+class YamlFileSource(SourceAsDict):
     def __init__(self, file_path: str, config_path: str = None) -> None:
-        with open(file_path) as f:
-            self._file_contents = safe_load(f)
-        if config_path:
-            self._data = dict_extractor(path=config_path, data=self._file_contents)
-        else:
-            self._data = self._file_contents
+        super().__init__(file_path, config_path)
 
-    def get_value(self, key: str) -> SourceResponse:
-        try:
-            response = dict_extractor(path=key, data=self._data)
-            return SourceResponse(
-                exists=True,
-                value=response
-            )
-        except KeyError:
-            return SourceResponse(
-                exists=False,
-                value=None
-            )
+    def _load_from_file(self, path: str) -> dict:
+        with open(path) as f:
+            return safe_load(f)
