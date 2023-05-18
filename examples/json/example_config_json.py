7bbf593afecad11d6865760fde0466cda3159786
Joao Pedro Goulart <joaopedromgoulart@gmail.com>
2023-05-17 23:29:04 -0300

    add exemplos json

--- examples/json/example_config_json.py
+++ examples/json/example_config_json.py
@@ -1,11 +1,11 @@
-from config_joker import Config, YamlFileSource
+from config_joker import Config, JsonFileSource
 
 
 def example():
     config = Config(
         sources=[
-            YamlFileSource(
-                file_path='./examples/yaml/config.yaml',
+            JsonFileSource(
+                file_path='./examples/json/config.json',
                 config_path='external_config_key[0].config'
             )
         ]
