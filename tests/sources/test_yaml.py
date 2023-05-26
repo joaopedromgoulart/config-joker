36eebcec8151e9ec178004e2f6e457b39d7f7184
Joao Pedro Goulart <joaopedromgoulart@gmail.com>
2023-05-25 23:03:33 -0300

    feat(config.Config): Add cast string to bool

--- tests/sources/test_yaml.py
+++ tests/sources/test_yaml.py
@@ -5,6 +5,6 @@ from config_joker.sources.yamlfile import YamlFileSource
 class TestYamlFileSourceGetValue:
     def test_load_from_file(self):
         file_data = 'config:\n  key: value'
-        with patch("builtins.open", mock_open(read_data=file_data)) as mock_file:
+        with patch("builtins.open", mock_open(read_data=file_data)):
             yaml_source = YamlFileSource(file_path='path')
         assert yaml_source._data == {'config': {'key': 'value'}}
